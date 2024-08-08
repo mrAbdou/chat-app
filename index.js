bd66c557e3ae032e57941efc4831e582f9d28e29
mrAbdou <abdesselemboubidi@gmail.com>
2024-08-08 11:09:29 +0200

    adjusting scripts to better and easy work
    this step meant to fix and make it automatic reload on every change on react to update the content of the build folder (static files of the express server)
    and the updates on the server it self

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+require("dotenv").config(); // --> to get access to variable environment file (.env)
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const path = require("path");
 const server = require("http").createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: [
+            `http://localhost:${process.env.PORT}`,
+        ],
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`client ${socket.id} connected successfully.`);
+})
 //static folder contains compiled react js code
+app.use(cors({
+    origin: [
+        `http://localhost:${process.env.PORT}`
+    ],
+    methods: ["GET", "POST"]
+}));
 app.use(express.static(path.join(__dirname, "build")));
 //serve the starter html file of react app
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "build", "index.html")));
 
-server.listen(3000, () => console.log(`listening on *:3000`));
+server.listen(process.env.PORT, () => console.log(`listening on *:${process.env.PORT}`));
