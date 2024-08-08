671c4b0aebd5e76e772f4c668afcfea61c45be65
mrAbdou <abdesselemboubidi@gmail.com>
2024-08-08 15:55:32 +0200

    CORRECTING SOME ERRORS
    -ADDING DEPLOY DOMAIN TO CORS ORIGINS ALLOWED TO CONNECT
    -ADDING ERROR HANDLING TO EXPRESS SERVER
    -ADDING API URL FOR DEV AND FOR PRODUCTION MODES AND USE NODE_ENV TO SELECT ONE OF THEM
    -RE-BUILD THE REACT APP CODE TO UPDATE BUILD FOLDER CONTENT ACCORDING THE ADJUSTMENTS DONE

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const server = require("http").createServer(app);
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
         origin: [
             `http://localhost:${process.env.PORT}`,
+            `https://chat-app-5fwe.onrender.com/`
         ],
         methods: ["GET", "POST"]
     }
@@ -18,18 +19,24 @@ io.on("connection", socket => {
     console.log(`client ${socket.id} connected successfully.`);
     socket.emit("connection", socket.id);
     socket.emit("available-messages", []); // this should get data from the database and send it to the new connected client
-    socket.on("send-message", newMessage => io.emit("send-message", {...newMessage, senderID: socket.id})); //should be inserted into the database too in here
+    socket.on("send-message", newMessage => io.emit("send-message", { ...newMessage, senderID: socket.id })); //should be inserted into the database too in here
     socket.on("delete-message", selectedMessageId => io.emit('delete-message', selectedMessageId)); // remove this message from the database in here too
 })
 //static folder contains compiled react js code
 app.use(cors({
     origin: [
-        `http://localhost:${process.env.PORT}`
+        `http://localhost:${process.env.PORT}`,
+        `https://chat-app-5fwe.onrender.com/`
     ],
     methods: ["GET", "POST"]
 }));
 //define the static folder to be used which is the build result of a react app
 app.use(express.static(path.join(__dirname, "build")));
+//ERROR HANDLING:
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 //serve the starter html file of react app
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "build", "index.html")));
 //redirect all other routes to the main root /
